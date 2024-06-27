e6b80bdf052114fe9ed89c3abb98c6b77cdd7a37
austinmargarone <austin@fcvt.dev>
2024-06-27 11:38:44 -0400

    fix: Coin List

--- src/components/data/list/CoinList10.tsx
+++ src/components/data/list/CoinList10.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import CoinList from "./CoinList";
 
-const TableOne = () => {
-  const maxCoinsToShow = 6;
+const CoinList10 = () => {
+  const maxCoinsToShow = 5;
 
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-        Top 10 Coins
+        Top 5 Coins
       </h4>
 
       <div className="flex flex-col overflow-x-auto">
@@ -17,4 +17,4 @@ const TableOne = () => {
   );
 };
 
-export default TableOne;
+export default CoinList10;
