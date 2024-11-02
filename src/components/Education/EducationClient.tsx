f670f44cae283b9ef8f7cc6b730d1987bf9bd13c
austinmargarone <austin@fcvt.dev>
2024-11-01 21:43:06 -0400

    Update Education Display

--- src/components/Education/EducationClient.tsx
+++ src/components/Education/EducationClient.tsx
@@ -10,16 +10,25 @@ interface EducationClientProps {
 
 export default function EducationClient({ posts }: EducationClientProps) {
   return (
-    <div>
-      {posts.map((post) => (
-        <article key={post.id} className="my-[2.5rem] flex flex-col">
-          <h2 className="text-red">{post.title}</h2>
-          <div
-            className="border"
-            dangerouslySetInnerHTML={{ __html: post.content.html }}
-          />
-        </article>
-      ))}
+    <div className="container mx-auto px-4">
+      <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-1">
+        {posts.map((post) => (
+          <div key={post.id} className="rounded-sm bg-white p-4 shadow-lg">
+            <a href={post.slug} target="_blank" rel="noopener noreferrer">
+              <h2 className="mb-2 text-xl font-semibold text-black">
+                {post.title}
+              </h2>
+              <p className="text-gray-600 mb-2">{post.title}</p>
+              <p className="cursor-pointer text-blue-500 underline">
+                Read more
+              </p>
+              <p className="text-gray-500 mt-2 text-sm">
+                {/* Date: {formatDate(article.createdAt)} */}
+              </p>
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
